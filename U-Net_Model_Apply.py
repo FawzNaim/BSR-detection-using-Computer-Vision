86fa985f0c64feca6af625a1b7984a34baeb2521
Fawz Naim <48981518+FawzNaim@users.noreply.github.com>
2026-08-14 16:39:32 -0400

    Update U-Net_Model_Apply.py

--- U-Net_Model_Apply.py
+++ U-Net_Model_Apply.py
@@ -365,8 +365,8 @@ def run_unet_inference(
 if __name__ == "__main__":
     # Option A: give the checkpoint path directly
     run_unet_inference(
-        image_path=r"/content/BSR-detection-using-Computer-Vision/BSR_extra_validation3.png",
-        mask_path=r"/content/BSR-detection-using-Computer-Vision/BSR_extra_validation3.png",
+        image_path=r"/content/BSR-detection-using-Computer-Vision/BSR_extra_validation4.png",
+        mask_path=r"/content/BSR-detection-using-Computer-Vision/BSR_extra_validation4.png",
         ckpt_path=r"/content/BSR-detection-using-Computer-Vision/models/bsr_unet_best_epoch_074.pth",
         size=512,
         thresh=0.5,
