afdb6c4d0d3816db611d078edc6d5882d7210103
Fawz Naim <48981518+FawzNaim@users.noreply.github.com>
2026-08-31 15:10:40 -0400

    Update Vgg-19_Model_Apply.py

--- Vgg-19_Model_Apply.py
+++ Vgg-19_Model_Apply.py
@@ -501,8 +501,8 @@ def run_vgg19_unet_inference(
 #  MAIN (example)
 # ===========================
 if __name__ == "__main__":
-    IMAGE_PATH = r"/content/BSR-detection-using-Computer-Vision/Non_BSR_1_GOM.png"
-    GT_MASK    = r"/content/BSR-detection-using-Computer-Vision/Non_BSR_1_GOM.png"
+    IMAGE_PATH = r"/content/BSR-detection-using-Computer-Vision/Non_BSR_2_GOM.png"
+    GT_MASK    = r"/content/BSR-detection-using-Computer-Vision/Non_BSR_2_GOM.png"
 
     # Option A: point directly to the checkpoint you want to test
     CKPT_PATH  = r"/content/BSR-detection-using-Computer-Vision/models/bsr_vgg19_best_epoch_042.pth"
